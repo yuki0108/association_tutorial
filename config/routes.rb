997c9d05850e01660247e65c8d72d48eac4a7764
先規　勇樹 <yukisaki80@gmail.com>
2021-11-16 23:04:52 +0900

    newアクションを実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  get 'tweets/new'
-  get 'tweets/index'
-  get 'tweets/show'
-  get 'tweets/create'
-  get 'users/index'
-  get 'users/show'
+  # get 'tweets/new'
+  # get 'tweets/index'
+  # get 'tweets/show'
+  # get 'tweets/create'
+  # get 'users/index'
+  # get 'users/show'
+  root 'tweets/index'
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :tweets
+  resources :users
 end
