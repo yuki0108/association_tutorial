c02b4e1cfad13de0ea7321eaa7450a6b3ebfd2f0
先規　勇樹 <yukisaki80@gmail.com>
2021-11-18 23:32:59 +0900

    ユーザーがツイートをお気に入りしたかどうかの判定メソッド

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,9 @@
 class Tweet < ApplicationRecord
   belongs_to :user
   has_many :favorites
+
+  # user_idカラムに「user.id」が入っているかチェック？
+  def favorited_user?(user)
+    favorites.where(user_id: user.id).exists?
+  end
 end
