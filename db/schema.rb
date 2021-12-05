38f50853f4b8b7a101c9b31eb6627adb4679275a
先規　勇樹 <yukisaki80@gmail.com>
2021-12-05 16:53:09 +0900

    comment用のルーティングを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_18_141518) do
+ActiveRecord::Schema.define(version: 2021_12_05_074529) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "tweet_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
